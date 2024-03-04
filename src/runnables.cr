230c7d4e49dabdf148c250b965b091ad1ef5c94f
Julien Portalier <julien@portalier.com>
2024-03-04 14:06:26 +0100

    Fix: math overflows in Runnables

--- src/runnables.cr
+++ src/runnables.cr
@@ -7,8 +7,8 @@ abstract class ExecutionContext
   # can be called from any thread in the execution context.
   class Runnables(N)
     def initialize(@global_queue : GlobalQueue)
-      @head = Atomic(Int32).new(0)
-      @tail = Atomic(Int32).new(0)
+      @head = Atomic(UInt32).new(0)
+      @tail = Atomic(UInt32).new(0)
       @buffer = uninitialized StaticArray(Fiber, N)
     end
 
@@ -21,7 +21,7 @@ abstract class ExecutionContext
     # def empty? : Bool
     #   head = @head.get(:relaxed)
     #   tail = @tail.get(:relaxed)
-    #   tail - head == 0
+    #   tail &- head == 0
     # end
 
     # Tries to push fiber on the local runnable queue. If the run queue is full,
@@ -33,12 +33,12 @@ abstract class ExecutionContext
         head = @head.get(:acquire) # sync with consumers
         tail = @tail.get(:relaxed)
 
-        if (tail - head) < N
+        if (tail &- head) < N
           # put fiber to local queue
           @buffer.to_unsafe[tail % N] = fiber
 
           # make the fiber available for consumption
-          @tail.set(tail + 1, :release)
+          @tail.set(tail &+ 1, :release)
           return
         end
 
@@ -51,16 +51,16 @@ abstract class ExecutionContext
     # Push fiber, along with a batch of fibers from local queue, on global queue.
     #
     # Executed only by the owner.
-    private def push_slow(fiber : Fiber, head : Int32, tail : Int32) : Bool
-      n = (tail - head) // 2
+    private def push_slow(fiber : Fiber, head : UInt32, tail : UInt32) : Bool
+      n = (tail &- head) // 2
       raise "BUG: queue is not full" if n != N // 2
 
       # first, try to grab a batch of fibers from local queue
       batch = uninitialized StaticArray(Fiber, N)
       n.times do |i|
-        batch.to_unsafe[i] = @buffer.to_unsafe[(head + i) % N]
+        batch.to_unsafe[i] = @buffer.to_unsafe[(head &+ i) % N]
       end
-      _, success = @head.compare_and_set(head, head + n, :acquire_release, :acquire)
+      _, success = @head.compare_and_set(head, head &+ n, :acquire_release, :acquire)
       return false unless success
 
       # append fiber to the batch
@@ -68,13 +68,13 @@ abstract class ExecutionContext
 
       # link the fibers
       n.times do |i|
-        batch.to_unsafe[i].schedlink = batch.to_unsafe[i + 1]
+        batch.to_unsafe[i].schedlink = batch.to_unsafe[i &+ 1]
       end
       queue = Queue.new(batch.to_unsafe[0], batch.to_unsafe[n])
 
       # now put the batch on global queue
       @global_queue.lock do
-        @global_queue.push(pointerof(queue), n + 1)
+        @global_queue.push(pointerof(queue), n &+ 1)
       end
 
       true
@@ -86,14 +86,14 @@ abstract class ExecutionContext
     #
     # Executed only by the owner.
     def push(queue : Queue*, size : Int32) : Nil
-      head = @head.get(:acquire) # sync with other consumers
-      tail = @tail.get(:relaxed)
+      tail = @tail.get(:acquire) # sync with other consumers
+      head = @head.get(:relaxed)
 
-      while !queue.value.empty? && (tail - head) < N
+      while !queue.value.empty? && (tail &- head) < N
         fiber = queue.value.pop
         @buffer.to_unsafe[tail % N] = fiber
-        tail += 1
-        size -= 1
+        tail &+= 1
+        size &-= 1
       end
 
       # make the fibers available for consumption
@@ -118,7 +118,7 @@ abstract class ExecutionContext
         return if tail == head
 
         fiber = @buffer.to_unsafe[head % N]
-        head, success = @head.compare_and_set(head, head + 1, :acquire_release, :acquire)
+        head, success = @head.compare_and_set(head, head &+ 1, :acquire_release, :acquire)
         return fiber if success
       end
     end
@@ -132,15 +132,15 @@ abstract class ExecutionContext
       n = src.grab(@buffer, tail)
       return if n == 0
 
-      n -= 1
-      fiber = @buffer.to_unsafe[(tail + n) % N]
+      n &-= 1
+      fiber = @buffer.to_unsafe[(tail &+ n) % N]
       return fiber if n == 0
 
       head = @head.get(:acquire) # sync with consumers
-      raise "BUG: local queue overflow" if tail - head + n >= N
+      raise "BUG: local queue overflow" if tail &- head &+ n >= N
 
       # make the fibers available for consumption
-      @tail.set(tail + n, :release)
+      @tail.set(tail &+ n, :release)
 
       fiber
     end
@@ -149,21 +149,21 @@ abstract class ExecutionContext
     # number of grabbed fibers.
     #
     # Can be executed by any scheduler.
-    protected def grab(buffer : StaticArray(Fiber, N), buffer_head : Int32) : Int32
+    protected def grab(buffer : StaticArray(Fiber, N), buffer_head : UInt32) : Int32
       head = @head.get(:acquire) # sync with other consumers
 
       loop do
         tail = @tail.get(:acquire) # sync with the producer
-        n = (tail - head) // 2
+        n = (tail &- head) // 2
 
         return 0 if n == 0 # queue is empty
         next if n > N // 2 # read inconsistent head and tail
 
         n.times do |i|
-          fiber = @buffer.to_unsafe[(head + i) % N]
-          buffer.to_unsafe[(buffer_head + i) % N] = fiber
+          fiber = @buffer.to_unsafe[(head &+ i) % N]
+          buffer.to_unsafe[(buffer_head &+ i) % N] = fiber
         end
-        head, success = @head.compare_and_set(head, head + n, :acquire_release, :acquire)
+        head, success = @head.compare_and_set(head, head &+ n, :acquire_release, :acquire)
         return n if success
       end
     end
