7d4be11480dc7c3da88eed5ed2e18959987c19e6
Julien Portalier <julien@portalier.com>
2024-04-18 18:36:00 +0200

    Refactor EC::Isolated to own the thread (really _one_ fiber)
    Instead of extending the EC::SingleThreaded, EC::Isolated is now a
    distinct implementation with a single thread and a single fiber (the
    thread's main fiber), while still being to wait on the event loop or
    communicate with other contexts through channels.
    
    Note: unlike other contexts, where threads loop and park themselves,
    the thread of isolated contexts will terminate as soon as the block
    terminates.

--- src/isolated.cr
+++ src/isolated.cr
@@ -1,32 +1,118 @@
 module ExecutionContext
-  # TODO: consider a specific implementation with a mere @runnext : Fiber? property
-  # TODO: shutdown the context when @fiber is dead
-  class Isolated < SingleThreaded
-    @spawn_context : ExecutionContext
-    private getter! fiber : Fiber
+  # Runs a single fiber, isolated on its own dedicated thread.
+  #
+  # Concurrency is disabled. Calls to `#spawn` will create fibers in another
+  # execution context (by default it's the default context). Any calls that
+  # result in waiting (e.g. sleep, or socket read/write) will block the thread
+  # since there are no other fibers to switch to.
+  #
+  # Isolated fibers can still communicate with other fibers running in other
+  # execution contexts using standard means, such as `Channel(T)` and `Mutex`.
+  #
+  # Example:
+  #
+  # ```
+  # ExecutionContext::Isolated.new("Gtk") { Gtk.main }
+  # ```
+  class Isolated
+    include ExecutionContext
+    include ExecutionContext::Scheduler
 
-    def initialize(name : String, @spawn_context = ExecutionContext.default, &block)
-      super name, hijack: false
-      @fiber = fiber = Fiber.new(name, self, &block)
-      enqueue fiber
+    getter event_loop : Crystal::EventLoop
+    getter thread : Thread
+    @main_fiber : Fiber?
+    getter name : String
+    getter? running : Bool = true
+
+    def initialize(@name : String, @spawn_context = ExecutionContext.default, &@func : ->)
+      @event_loop = Crystal::EventLoop.create
+      @thread = uninitialized Thread
+
+      wg = Thread::WaitGroup.new(1)
+
+      Thread.new(@name) do |thread|
+        @thread = thread
+        thread.execution_context = self
+        thread.current_scheduler = self
+        thread.main_fiber.name = @name
+        @main_fiber = thread.main_fiber
+        wg.done
+
+        run
+      end
+
+      wg.wait
     end
 
+    @[AlwaysInline]
+    def execution_context : Isolated
+      self
+    end
+
+    def stack_pool : Fiber::StackPool
+      raise NotImplementedError.new("No stack pool for isolated contexts")
+    end
+
+    @[AlwaysInline]
     def spawn(*, name : String? = nil, &block : ->) : Fiber
       @spawn_context.spawn(name: name, &block)
     end
 
-    @[Deprecated]
+    # :nodoc:
+    @[AlwaysInline]
     def spawn(*, name : String? = nil, same_thread : Bool, &block : ->) : Fiber
       raise ArgumentError.new("#{self.class.name}#spawn doesn't support same_thread:true") if same_thread
       @spawn_context.spawn(name: name, &block)
     end
 
     def enqueue(fiber : Fiber) : Nil
-      if fiber == @fiber || fiber == @main_fiber
-        super
+      if fiber == @main_fiber
+        if ExecutionContext.current == self
+          # local enqueue: the event loop is the one pushing
+        else
+          # cross context communication (e.g. channel, mutex)
+          @event_loop.interrupt_loop
+        end
       else
+        # concurrency is disabled
         raise RuntimeError.new("Can't enqueue #{fiber} in #{self}")
       end
+    end
+
+    protected def reschedule : Nil
+      Crystal.trace "sched:reschedule"
+
+      # always block, even when there's nothing to do; the fiber got rescheduled
+      # so we likely wait on cross context communication, like a channel:
+      @event_loop.run(blocking: true, block_when_empty: true)
+
+      Crystal.trace "sched:resume"
+    end
+
+    protected def resume(fiber : Fiber) : Nil
+      raise RuntimeError.new("Can't resume #{fiber} in #{self}")
+    end
+
+    private def run
+      @func.call
+    rescue ex
+      message = "BUG: %s#run crashed with %s"
+      Crystal::System.print_error_buffered(message, self.class.name, ex.class.name, backtrace: ex.backtrace)
+    ensure
+      @running = false
+    end
+
+    @[AlwaysInline]
+    def inspect(io : IO) : Nil
+      to_s(io)
+    end
+
+    def to_s(io : IO) : Nil
+      io << "#<" << self.class.name << ":0x"
+      object_id.to_s(io, 16)
+      io << ' ' << name << " thread=0x"
+      thread.@system_handle.to_s(io, 16)
+      io << '>'
     end
   end
 end
