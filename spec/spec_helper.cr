433f40e20b0c15f703be2885337b37a1c351037f
Julien Portalier <julien@portalier.com>
2024-05-06 18:17:30 +0200

    Fix: Thread.sleep for windows

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,19 +1,25 @@
 require "spec"
 require "../src/execution_context"
 
-lib LibC
-  fun nanosleep(Timespec*, Timespec*) : UInt
-end
+{% unless flag?(:win32) %}
+  lib LibC
+    fun nanosleep(Timespec*, Timespec*) : UInt
+  end
+{% end %}
 
 class Thread
   def self.sleep(span : Time::Span) : Nil
-    ts = uninitialized LibC::Timespec
-    ts.tv_sec = typeof(ts.tv_sec).new(span.seconds)
-    ts.tv_nsec = typeof(ts.tv_nsec).new(span.nanoseconds)
+    {% if flag?(:win32) %}
+      LibC.Sleep(span.total_milliseconds.to_i.clamp(1..))
+    {% else %}
+      ts = uninitialized LibC::Timespec
+      ts.tv_sec = typeof(ts.tv_sec).new(span.seconds)
+      ts.tv_nsec = typeof(ts.tv_nsec).new(span.nanoseconds)
 
-    if LibC.nanosleep(pointerof(ts), out rem) == -1
-      raise RuntimeError.from_errno("nanosleep") unless Errno.value == Errno::EINTR
-    end
+      if LibC.nanosleep(pointerof(ts), out rem) == -1
+        raise RuntimeError.from_errno("nanosleep") unless Errno.value == Errno::EINTR
+      end
+    {% end %}
   end
 end
 
