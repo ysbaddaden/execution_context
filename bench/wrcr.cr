059ca5c93d8f575bc5381b509adec2663fe1e8d9
Beta Ziliani <beta@manas.tech>
2024-09-06 14:00:19 -0300

    Crystal version of wrk

---
+++ bench/wrcr.cr
@@ -0,0 +1,74 @@
+{% if flag?(:ec) %}
+  require "../src/execution_context"
+{% end %}
+require "http/client"
+require "option_parser"
+
+
+address = "localhost:8080"
+duration = 30.seconds
+requests = 12
+fibers = 20
+
+OptionParser.parse do |parser|
+  parser.banner = "Usage: wrcr [arguments]\nNote: The number of threads is set by the CRYSTAL_WORKERS environment variable."
+  parser.on("-f NUMBER", "--fibers=NUMBER", "Number of total fibers. Defaults to #{fibers}") { |fbrs| fibers = fbrs.to_i }
+  parser.on("-r NUMBER", "--requests=NUMBER", "Number of requests before closing the connection. Defaults to #{requests}") { |req| requests = req.to_i }
+  parser.on("-d NUMBER", "--duration=NUMBER", "Duration of the test. Defaults to #{duration} seconds") { |dur| duration = dur.to_i.seconds }
+  parser.on("-u URL", "--url=URL", "Specifies the address of the server. Defaults to #{address}") { |url| address = url }
+  parser.on("-h", "--help", "Show this help") do
+    puts parser
+    exit
+  end
+  parser.invalid_option do |flag|
+    STDERR.puts "ERROR: #{flag} is not a valid option."
+    STDERR.puts parser
+    exit(1)
+  end
+end
+
+address = address.gsub("http://", "")
+if address.includes? ":"
+  address, port_s = address.split(":")
+  port = port_s.to_i
+else
+  port = 80
+end
+
+start = Time.local
+
+successes = Channel({Int32,Int64}).new(fibers)
+
+fibers.times do
+  spawn do
+    j = 0
+    bytes = 0_i64
+    client = HTTP::Client.new address, port
+    loop do
+      requests.times do
+        break if Time.local - start > duration
+        response = client.get "/"
+        if response.status_code == 200
+          j += 1
+          bytes += response.body.size
+        end
+      rescue IO::TimeoutError
+      rescue e
+        STDERR.puts e
+      end
+      break if Time.local - start > duration
+    end
+    successes.send({j, bytes})
+  end
+end
+
+total_successes = 0
+total_bytes = 0_i64
+
+fibers.times do
+  reqs, bytes = successes.receive
+  total_successes += reqs
+  total_bytes += bytes
+end
+
+puts "Total requests: #{total_successes}\nTotal bytes: #{total_bytes.humanize}"
