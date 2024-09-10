1a805014bccb45effc3a7abcc04005c6d24804fb
Beta Ziliani <beta@manas.tech>
2024-09-10 14:38:54 -0300

    Added three modalities: -Dpreview_mt and -Dpreview_mt -Dec -Dmt are multithreaded environments, the original and EC one. -Dpreview_mt -Dec utilizes the main thread for handling connections, and another MTEC to handle responses

--- bench/http_dispatch.cr
+++ bench/http_dispatch.cr
@@ -1,4 +1,10 @@
 # A server listening on port 8080 that dispatches requests to different handlers
+#
+# With the `ec` flag, the server uses two ExecutionContexts to manage the threads,
+# SingleThreaded one for accepting connections, and MultiThreaded for processing responses.
+#
+# With the `mt` flag, the server uses a single ExecutionContext for both accepting connections
+# and processing responses.
 {% if flag?(:ec) %}
   require "../src/execution_context"
 {% end %}
@@ -6,23 +12,23 @@ require "socket"
 require "http/server/request_processor"
 
 THREAD_COUNT = (ENV["CRYSTAL_WORKERS"]?.try(&.to_i?) || 4) - 1 # 1 thread is the dedicated thread
-MSG_SIZE = ENV["BENCH_MSG_SIZE"]?.try(&.to_i?) || 1000
-MSG = "a" * MSG_SIZE
+MSG_SIZE     = ENV["BENCH_MSG_SIZE"]?.try(&.to_i?) || 1000
+MSG          = "a" * MSG_SIZE
 
 class Dispatcher
-  {% if flag?(:ec) %}
+  {% if flag?(:ec) && !flag?(:mt) %}
     @mt : ExecutionContext::MultiThreaded? = nil
   {% end %}
 
   def initialize(@processor : HTTP::Server::RequestProcessor)
-    {% if flag?(:ec) %}
+    {% if flag?(:ec) && !flag?(:mt) %}
       @mt = ExecutionContext::MultiThreaded.new("MT", (THREAD_COUNT..THREAD_COUNT))
     {% end %}
   end
 
   def dispatch(io)
     {% begin %}
-    {% if flag?(:ec) %}
+    {% if flag?(:ec) && !flag?(:mt) %}
       @mt.not_nil!.spawn do
     {% else %}
       spawn do
@@ -33,14 +39,14 @@ class Dispatcher
       end
     {% end %}
   end
-  
 end
 
-{% if flag?(:ec) %}
+{% if flag?(:ec) && !flag?(:mt) %}
   # Sets the main thread to be part of the SingleThreaded execution context
+  # TODO: I think this is unnecessary, and with `ec` but no `mt`, the main thread
+  # is already part of the SingleThreaded execution context
   ExecutionContext::SingleThreaded.default
 {% end %}
-
 
 socket = TCPServer.new(Socket::IPAddress::LOOPBACK, 8080, reuse_port: true)
 processor = HTTP::Server::RequestProcessor.new do |context|
