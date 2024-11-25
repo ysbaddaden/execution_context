236677b63c3bc6b3f5302d099d6fe8e522aa6e0b
Julien Portalier <julien@portalier.com>
2024-11-25 11:04:24 +0100

    Cleanup

--- src/core_ext/thread.cr
+++ src/core_ext/thread.cr
@@ -25,44 +25,6 @@ class Thread
     end
   end
 
-  # the following methods apply these patches:
-  # https://github.com/crystal-lang/crystal/pull/14558
-
-  # def initialize(@name : String? = nil, &@func : Thread ->)
-  #   @system_handle = uninitialized Crystal::System::Thread::Handle
-  #   init_handle
-
-  #   Thread.threads.push(self) # <= moved here
-  # end
-
-  # protected def start
-  #   Thread.current = self
-  #   @current_fiber = @main_fiber = fiber = Fiber.new(stack_address, self)
-
-  #   if name = @name
-  #     self.system_name = name
-  #   end
-
-  #   begin
-  #     @func.call(self)
-  #   rescue ex
-  #     @exception = ex
-  #   ensure
-  #     {% if flag?(:preview_mt) %}
-  #       # fix the thread stack now so we can start cleaning up references
-  #       GC.lock_read
-  #       GC.set_stackbottom(self.gc_thread_handler, fiber.@stack_bottom)
-  #       GC.unlock_read
-  #     {% else %}
-  #       GC.set_stackbottom(fiber.@stack_bottom)
-  #     {% end %}
-
-  #     Thread.threads.delete(self)
-  #     Fiber.inactive(fiber)
-  #     detach { system_close }
-  #   end
-  # end
-
   def scheduler : Crystal::Scheduler
     raise "BUG: deprecated call to Thread#scheduler"
   end
